5d3f1bf1ac07504b159ec8148aa69b05939f6b6c
aveabina <bainabadmaeva2002@gmail.com>
2022-03-11 01:03:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/playmusic/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.playmusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button btnStart = findViewById(R.id.button_start);
+        final Button btnStop = findViewById(R.id.button_stop);
+
+        // запуск службы
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // используем явный вызов службы
+                startService(
+                        new Intent(MainActivity.this, MyService.class));
+                }
+            });
+        // остановка службы
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(
+                        new Intent(MainActivity.this, MyService.class));
+                }
+            });
+        }
+    }
